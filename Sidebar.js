c89e79943c6773646fab41e4ba025648e3475cfd
mysticentity <36752387+mysticentity@users.noreply.github.com>
2022-07-24 09:45:50 -0700

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,7 @@
+<body>
+    <script>
+        function moveChatbox(){
+            var moveChatbox = document.getElementById("chatContent-3KubbW").style.left;
+            moveChatbox.style.left = moveChatbox.substring(0,moveChatbox.length-1) + 50.toString() + "px";
+        }
+    </script>
