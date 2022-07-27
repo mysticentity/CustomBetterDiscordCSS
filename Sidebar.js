7ddf04070b80c66687216069fd6e0755e34e15dd
mysticentity <36752387+mysticentity@users.noreply.github.com>
2022-07-27 14:26:03 -0700

    Delete Sidebar.js

--- Sidebar.js
+++
@@ -1,7 +0,0 @@
-<body>
-    <script>
-        function moveChatbox(){
-            var moveChatbox = document.getElementById("chatContent-3KubbW").style.left;
-            moveChatbox.style.left = moveChatbox.substring(0,moveChatbox.length-1) + 50.toString() + "px";
-        }
-    </script>
